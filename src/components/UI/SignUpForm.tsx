0b4fccc5ef36cb1ef284801c89d443ecde6851e7
Roisfaozi <roisfaozi55@gmail.com>
2023-10-15 03:02:37 +0700

    * feat(SignUpForm.tsx): add form validation and error handling for email and password fields * fix(SignUpForm.tsx): fix onSubmit function name in handleSubmit * fix(SignUpForm.tsx): fix error message display for email and password fields

--- src/components/UI/SignUpForm.tsx
+++ src/components/UI/SignUpForm.tsx
@@ -1,16 +1,101 @@
 'use client'
+import { ChangeEventHandler, useState } from 'react';
 import Input from '../utils/Input';
 
 interface SignUpFormProps {
   onSubmit: () => void; // Ganti dengan fungsi penanganan pengiriman formulir yang sesuai
 }
 
-function SignUpForm({ onSubmit }: SignUpFormProps) {
+function SignUpForm() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = userInfo;
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+
+    // Validasi email saat pengguna mengetik
+    if (name === "email") {
+      if (!isValidEmail(value) && value !== '') {
+        setError({ ...error, email: "Invalid email format. Missing @ in the email address" });
+      } else {
+        setError({ ...error, email: "" });
+      }
+    }
+
+    // Validasi password saat pengguna mengetik
+    if (name === "password") {
+      if (value.length < 6 && value !== '') {
+        setError({ ...error, password: "Password must be at least 6 characters" });
+      } else {
+        setError({ ...error, password: "" });
+      }
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Cek apakah masih ada pesan kesalahan
+    if (error.email || error.password) {
+      return;
+    }
+
+    // Lanjutkan dengan penanganan pengiriman formulir jika semuanya valid
+    // onSubmit(); // Uncomment this line to handle form submission
+  };
+
+  const isValidEmail = (email: string) => {
+    // Validasi email dengan ekspresi reguler
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(email);
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <Input label="Name" id="name" type="text" placeholder="Enter your name" required />
-      <Input label="Email" id="email" type="email" placeholder="Enter your email address" required />
-      <Input label="Password" id="password" type="password" placeholder="Enter your password" required />
+    <form onSubmit={handleSubmit}>
+      <Input
+        value={name}
+        label="Name"
+        name="name"
+        id="name"
+        type="text"
+        placeholder="Enter your name"
+        required
+        onChange={handleChange}
+      />
+      <Input
+        value={email}
+        label="Email"
+        name="email"
+        id="email"
+        type="email"
+        placeholder="Enter your email address"
+        required
+        onChange={handleChange}
+      />
+      <p className={`text-red-500 text-sm mb-4 h-1`}>{error.email}</p>
+      <Input
+        value={password}
+        label="Password"
+        id="password"
+        type="password"
+        placeholder="Enter your password"
+        required
+        onChange={handleChange}
+        name="password"
+      />
+      <p className={`text-red-500 text-sm mb-4 h-2`}>{error.password}</p>
       <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full" type="submit">Sign up</button>
       <div className="text-sm text-gray-500 text-center mt-3">
         By creating an account, you agree to the <a className="underline" href="#0">terms & conditions</a>, and our <a className="underline" href="#0">privacy policy</a>.
