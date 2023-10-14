6e9df05c622b52262176c453911b11272c625e2e
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 20:48:59 +0700

    * style(Header.tsx): update header component styling and structure

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -2,14 +2,12 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="header">
-      <nav>
-        <div className="container">
-          <Link href="/">Your App Name</Link>
-          <ul>
-            <li><Link href="/login">Login</Link></li>
-          </ul>
-        </div>
+    <header className="bg-blue-500 py-4">
+      <nav className="container mx-auto flex items-center justify-between">
+        <Link href="/" className="text-white text-xl font-semibold">Your App Name</Link>
+        <ul className="flex space-x-4">
+          <li><Link href="/login" className="text-white">Login</Link></li>
+        </ul>
       </nav>
     </header>
   );
