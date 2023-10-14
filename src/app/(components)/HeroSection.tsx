d70c2d206332d1415871e5c91baaa4f64265c920
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 17:01:39 +0700

    * feat(HeroSection.tsx): add HeroSection component to the app/components directory

---
+++ src/app/(components)/HeroSection.tsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="hero-content">
+          <h1>Welcome to Your App Name</h1>
+          <p>Your App Slogan or Tagline</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
