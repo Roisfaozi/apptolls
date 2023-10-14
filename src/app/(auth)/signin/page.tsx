dad4d594ac1c377900e947b4efac8a71def78d9f
Roisfaozi <roisfaozi55@gmail.com>
2023-10-15 03:11:05 +0700

    * refactor(signin/page.tsx): extract sign in form into a separate component * fix(signin/page.tsx): remove commented out form code

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@ export const metadata = {
   description: 'Page description',
 }
 
+import SignInForm from '@/components/UI/SigninForm'
 import Link from 'next/link'
 
 export default function SignIn() {
@@ -18,38 +19,7 @@ export default function SignIn() {
 
           {/* Form */}
           <div className="max-w-sm mx-auto">
-            <form>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password</label>
-                    <Link href="/reset-password" className="text-sm font-medium text-blue-600 hover:underline">Having trouble signing in?</Link>
-                  </div>
-                  <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Enter your password" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="flex items-center">
-                      <input type="checkbox" className="form-checkbox" />
-                      <span className="text-gray-600 ml-2">Keep me signed in</span>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign in</button>
-                </div>
-              </div>
-            </form>
+            <SignInForm />
             <div className="flex items-center my-6">
               <div className="border-t border-gray-300 grow mr-3" aria-hidden="true"></div>
               <div className="text-gray-600 italic">Or</div>
