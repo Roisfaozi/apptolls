b201656d742d19f34c3ca25c7c4c05ab38418185
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 20:49:11 +0700

    * refactor(Footer.tsx): update footer component to use tailwind CSS classes and improve readability

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -1,18 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-content">
-          <div className="footer-logo">
-            <p>Your App Name</p>
-          </div>
-          <div className="footer-links">
-            <ul>
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/contact">Contact Us</a></li>
-              <li><a href="/privacy-policy">Privacy Policy</a></li>
-            </ul>
-          </div>
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="text-2xl font-semibold">Your App Name</div>
+          <ul className="flex space-x-6">
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="/privacy-policy">Privacy Policy</a></li>
+          </ul>
         </div>
       </div>
     </footer>
