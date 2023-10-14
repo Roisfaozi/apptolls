66d9288ff64fa4b4c0eb3274950d340410332caf
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 23:56:50 +0700

    * chore(layout.tsx): update font import to include variable, display, and swap * feat(layout.tsx): add Header component and update body class with additional styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
-import './globals.css'
+import Header from '@/components/UI/Header'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './css/style.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
+        <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
