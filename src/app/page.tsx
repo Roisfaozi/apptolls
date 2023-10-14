8e1e14e7edad0403ae394ab6635f370fae4b25f1
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 20:48:02 +0700

    * feat(page.tsx): add Head component for setting page title, description, and favicon * feat(page.tsx): add Header, HeroSection, FeaturesSection, and Footer components * feat(page.tsx): add Call-to-Action (CTA) section with app purchase link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
+import Head from 'next/head';
+import FeaturesSection from './(components)/FeaturesSection';
+import Footer from './(components)/Footer';
+import Header from './(components)/Header';
+import HeroSection from './(components)/HeroSection';
 
 export default function Home() {
   return (
     <main >
-      <Home />
+      <div>
+        <Head>
+          <title>Your App Name</title>
+          <meta name="description" content="Promote your app with Next.js" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Header />
+
+        <main>
+          <HeroSection />
+          <FeaturesSection />
+        </main>
+
+        {/* Add a Call-to-Action (CTA) for app purchase */}
+        <section className="cta-section">
+          <div className="container">
+            <h2>Get Started with Our App Today!</h2>
+            <p>Experience the power of our app and supercharge your productivity.</p>
+            <a href="/purchase" className="cta-button">Buy Now</a>
+          </div>
+        </section>
+        <Footer />
+      </div>
     </main>
   )
 }
