3f4f81d6722b1ce7ca7e306c22260d59561a59aa
Roisfaozi <roisfaozi55@gmail.com>
2023-10-15 00:39:50 +0700

    * feat(signup): add form functionality to handle user input and submission * fix(signup): fix typo in form element

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,10 +2,38 @@ export const metadata = {
   title: 'Sign Up - Simple',
   description: 'Page description',
 }
-
-import Link from 'next/link'
+import Link from 'next/link';
+import { ChangeEventHandler, useState } from 'react';
 
 export default function SignUp() {
+
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = userInfo
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    const { name, value } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const oldUser = await UserModel.findOne({ email: userInfo.email });
+    if (oldUser) {
+      return alert("Email sudah digunakan!");
+    }
+
+    const user = await UserModel.create({ ...userInfo });
+
+    useNavigate("/login");
+  };
+
+
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -18,39 +46,12 @@ export default function SignUp() {
 
           {/* Form */}
           <div className="max-w-sm mx-auto">
-            <form>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name <span className="text-red-600">*</span></label>
-                  <input id="name" type="text" className="form-input w-full text-gray-800" placeholder="Enter your name" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password <span className="text-red-600">*</span></label>
-                  <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Enter your password" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign up</button>
-                </div>
-              </div>
-              <div className="text-sm text-gray-500 text-center mt-3">
-                By creating an account, you agree to the <a className="underline" href="#0">terms & conditions</a>, and our <a className="underline" href="#0">privacy policy</a>.
-              </div>
-            </form>
+            <SignUp />
             <div className="flex items-center my-6">
               <div className="border-t border-gray-300 grow mr-3" aria-hidden="true"></div>
               <div className="text-gray-600 italic">Or</div>
               <div className="border-t border-gray-300 grow ml-3" aria-hidden="true"></div>
-            </div>
+              s</div>
             <form>
               <div className="flex flex-wrap -mx-3 mb-3">
                 <div className="w-full px-3">
