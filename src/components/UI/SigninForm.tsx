32f7afb09ec017f6592eb9b042cdfa422f3dea3a
Roisfaozi <roisfaozi55@gmail.com>
2023-10-15 06:04:53 +0700

    * chore(SigninForm.tsx): refactor SigninForm component * - Remove unused onSubmit prop from SignInForm component * - Add name and value props to Input components * - Add onChange event handler to Input components * - Remove redundant label for password input field * - Move link to reset password below password input field * - Remove redundant div wrapper for password input field and checkbox

--- src/components/UI/SigninForm.tsx
+++ src/components/UI/SigninForm.tsx
@@ -1,27 +1,25 @@
 'use client'
 import Link from 'next/link';
+import { ChangeEvent } from 'react';
 import Input from '../utils/Input';
 
-interface SignInFormProps {
-  onSubmit: () => void; // Ganti dengan fungsi penanganan pengiriman formulir yang sesuai
-}
 
-function SignInForm({ onSubmit }: SignInFormProps) {
+function SignInForm() {
   return (
-    <form onSubmit={onSubmit}>
-      <Input label="Email" id="email" type="email" placeholder="Enter your email address" required />
+    <form >
+      <Input label="Email" id="email" type="email" placeholder="Enter your email address" required name='email' value={'name'} onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+        throw new Error('Function not implemented.');
+      }} />
+
+      <Input id="password" type="password" placeholder="Enter your password" required label="Password" name='password' value={'password'} onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+        throw new Error('Function not implemented.');
+      }} />
+
       <div className="flex flex-wrap -mx-3 mb-4">
         <div className="w-full px-3">
+
           <div className="flex justify-between">
-            <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password</label>
             <Link href="/reset-password" className="text-sm font-medium text-blue-600 hover:underline">Having trouble signing in?</Link>
-          </div>
-          <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Enter your password" required />
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-4">
-        <div className="w-full px-3">
-          <div className="flex justify-between">
             <label className="flex items-center">
               <input type="checkbox" className="form-checkbox" />
               <span className="text-gray-600 ml-2">Keep me signed in</span>
