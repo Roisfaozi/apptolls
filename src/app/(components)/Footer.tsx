bf4390f99d2b9f682657184bb37a531cb6e993f4
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 17:02:04 +0700

    * feat(Footer.tsx): add Footer component

---
+++ src/app/(components)/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-logo">
+            <p>Your App Name</p>
+          </div>
+          <div className="footer-links">
+            <ul>
+              <li><a href="/about">About Us</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+              <li><a href="/privacy-policy">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
