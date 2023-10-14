cd779f5971bdc954af821c7a1af2ca3b804547f5
Roisfaozi <roisfaozi55@gmail.com>
2023-10-14 17:01:53 +0700

    * feat(FeaturesSection.tsx): add FeaturesSection component to the app/components directory

---
+++ src/app/(components)/FeaturesSection.tsx
@@ -0,0 +1,23 @@
+const FeaturesSection = () => {
+  return (
+    <section className="features-section">
+      <div className="container">
+        <h2>Key Features</h2>
+        <div className="feature">
+          <h3>Feature 1</h3>
+          <p>Explanation of Feature 1.</p>
+        </div>
+        <div className="feature">
+          <h3>Feature 2</h3>
+          <p>Explanation of Feature 2.</p>
+        </div>
+        <div className="feature">
+          <h3>Feature 3</h3>
+          <p>Explanation of Feature 3.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesSection;
