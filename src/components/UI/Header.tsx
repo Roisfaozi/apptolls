0e4da5dfd0fef9aaa6582e130796b63ff94014e2
Roisfaozi <roisfaozi55@gmail.com>
2023-10-16 03:35:58 +0700

    * refactor(Header.tsx): * - Move import statement for `signOut` and `useSession` from `next-auth/react` to the top of the file * - Remove commented out code for detecting scroll position * - Refactor desktop navigation section to conditionally render sign in and sign up links based on authentication status * - Add conditional rendering for user profile link and logout button when user is authenticated

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 
 import Link from 'next/link'
@@ -8,9 +9,10 @@ import MobileMenu from './Mobile-menu'
 
 export default function Header() {
 
+  const { data, status } = useSession()
+  const isAuth = status === 'authenticated'
   const [top, setTop] = useState<boolean>(true)
 
-  // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.scrollY > 10 ? setTop(false) : setTop(true)
   }
@@ -25,27 +27,44 @@ export default function Header() {
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-white backdrop-blur-sm shadow-lg' : ''}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
-
-          {/* Site branding */}
           <div className="shrink-0 mr-4">
             <Logo />
           </div>
 
-          {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
-            {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-              </li>
-              <li>
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
-                </Link>
-              </li>
+              {
+                isAuth ? (
+                  <>
+                    <li>
+                      <Link href="/profile" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">{data?.user?.name}</Link>
+                    </li>
+                    <li>
+                      <button className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" onClick={() => signOut()}>
+                        <span>Logout</span>
+                        <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                        </svg>
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                    </li>
+                    <li>
+                      <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                        <span>Sign up</span>
+                        <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                        </svg>
+                      </Link>
+                    </li>
+                  </>
+                )
+              }
+
             </ul>
 
           </nav>
