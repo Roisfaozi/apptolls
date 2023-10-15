d6bafee4ef1431e98dc88d218f6a9142de4e9113
Roisfaozi <roisfaozi55@gmail.com>
2023-10-16 03:17:33 +0700

    * feat(Alert.tsx): add value prop to Alert component

--- src/components/utils/Alert.tsx
+++ src/components/utils/Alert.tsx
@@ -1,5 +1,5 @@
 
-function Alert() {
+function Alert({ value }: { value: string }) {
   return (
     <div className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
       <svg className="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -7,7 +7,7 @@ function Alert() {
       </svg>
       <span className="sr-only">Info</span>
       <div>
-        <span className="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+        {value}
       </div>
     </div>
   )
