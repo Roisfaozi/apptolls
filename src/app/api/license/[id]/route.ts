2ae17e124ace5711588f5680cdde15c404ccebe3
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 02:47:34 +0700

    * feat(route.ts): add GET route for retrieving license information by ID

---
+++ src/app/api/license/[id]/route.ts
@@ -0,0 +1,34 @@
+import startDb from '@/lib/db'
+import { getAuthSession } from '@/lib/nextauth-options'
+import { LicenseModel } from '@/models/licenseModels'
+import { NextResponse } from 'next/server'
+
+export const GET = async (
+  req: Request,
+  context: { params: { id: string } }
+): Promise<Response> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    await startDb()
+    const id = context.params.id
+    const license = await LicenseModel.findOne({ _id: id })
+      .populate({ path: 'user_id', select: 'id name' })
+      .populate({ path: 'product_id', select: 'id name' })
+    if (!license) {
+      return NextResponse.json({ error: 'License not found' }, { status: 404 })
+    }
+
+    return NextResponse.json({
+      license,
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new license' },
+      { status: 500 }
+    )
+  }
+}
