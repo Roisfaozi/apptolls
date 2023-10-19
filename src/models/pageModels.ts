a16c09c48e36d9f3be040d857ae0ff4d3fe53aaf
Roisfaozi <roisfaozi55@gmail.com>
2023-10-19 20:35:45 +0700

    * chore(pageModels.ts): change variable names from contentId to content_id and userId to user_id in pageSchema

--- src/models/pageModels.ts
+++ src/models/pageModels.ts
@@ -2,14 +2,14 @@ import mongoose, { Document, Model, ObjectId, Schema } from 'mongoose'
 const { ObjectId } = mongoose.Schema
 interface IPage extends Document {
   pageId: string
-  contentId: ObjectId
-  userId: ObjectId
+  content_id: ObjectId
+  user_id: ObjectId
 }
 
 const pageSchema: Schema<IPage> = new Schema({
   pageId: { type: String, required: true },
-  contentId: [{ type: ObjectId, ref: 'Content' }],
-  userId: { type: ObjectId, required: true, ref: 'User' },
+  content_id: [{ type: ObjectId, ref: 'Content' }],
+  user_id: { type: ObjectId, required: true, ref: 'User' },
 })
 
 const PageModel: Model<IPage> = mongoose.model<IPage>('Page', pageSchema)
