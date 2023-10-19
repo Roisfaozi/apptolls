054f2b202fc0be672a026eb79997e44db86683ed
Roisfaozi <roisfaozi55@gmail.com>
2023-10-19 17:37:31 +0700

    * fix(route.ts): add request parameter to GET, PUT, and DELETE functions * feat(route.ts): add PUT and DELETE functions for updating and deleting products

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -2,11 +2,14 @@ import startDb from '@/lib/db'
 import { getAuthSession } from '@/lib/nextauth-options'
 import { ProductModel } from '@/models/productModels'
 import { NextResponse } from 'next/server'
-import { NewResponse } from '../route'
+import { NewProductRequest, NewResponse } from '../route'
 
-export const GET = async (context: {
-  params: { id: string }
-}): Promise<NewResponse> => {
+export const GET = async (
+  req: Request,
+  context: {
+    params: { id: string }
+  }
+): Promise<NewResponse> => {
   try {
     const session = await getAuthSession()
 
@@ -14,11 +17,9 @@ export const GET = async (context: {
       return new NextResponse('Unauthorized', { status: 401 })
     }
     await startDb()
-
     const id = context.params.id
     const product = await ProductModel.findById(id)
 
-    console.log(id)
     if (!product) {
       return NextResponse.json({ error: 'Product not found' }, { status: 404 })
     }
@@ -35,6 +36,79 @@ export const GET = async (context: {
     console.error(error)
     return NextResponse.json(
       { error: 'Failed to fetch products' },
+      { status: 500 }
+    )
+  }
+}
+
+export const PUT = async (
+  req: Request,
+  context: {
+    params: { id: string }
+  }
+): Promise<NewResponse> => {
+  try {
+    const session = await getAuthSession()
+    await startDb()
+
+    if (!session?.user) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+    const id = context.params.id
+    const productData = (await req.json()) as NewProductRequest
+    const product = await ProductModel.findById(id)
+
+    if (!product) {
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 })
+    }
+
+    product.name = productData.name
+    product.description = productData.description
+    product.price = productData.price
+
+    await product.save()
+
+    return NextResponse.json({
+      product: {
+        id: product._id.toString(),
+        name: product.name,
+        description: product.description,
+        price: product.price,
+      },
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Failed to update the product' },
+      { status: 500 }
+    )
+  }
+}
+
+export const DELETE = async (context: {
+  params: { id: string }
+}): Promise<NewResponse> => {
+  try {
+    const session = await getAuthSession()
+    console.log(session)
+    if (!session?.user) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+    await startDb()
+    const id = context.params.id
+    const product = await ProductModel.findById(id)
+
+    if (!product) {
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 })
+    }
+
+    await product.deleteOne()
+
+    return NextResponse.json({ message: 'Product deleted successfully' })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Failed to delete the product' },
       { status: 500 }
     )
   }
