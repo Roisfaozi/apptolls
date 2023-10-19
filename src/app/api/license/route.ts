d4818c1acea6e1e2bf3378218ec900ca57455422
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 02:29:47 +0700

    * feat(route.ts): add POST method to create a new license * fix(route.ts): fix typo in error message

---
+++ src/app/api/license/route.ts
@@ -0,0 +1,77 @@
+import startDb from '@/lib/db'
+import { getAuthSession } from '@/lib/nextauth-options'
+import { LicenseModel } from '@/models/licenseModels'
+import { ProductModel } from '@/models/productModels'
+import generateLicenseKey from '@/utils/generateLicense'
+import { NextResponse } from 'next/server'
+
+export interface NewLicenseRequest {
+  licenseKey: string
+  product_id: string
+  purchasedAt: Date
+  lastChecked: Date
+  user_id: string
+}
+
+export interface NewLicenseResponse {
+  id: string
+  licenseKey: string
+  product_id: string
+  purchasedAt: Date
+  lastChecked: Date
+  user_id: string
+}
+
+export type NewResponse = NextResponse<{
+  license?: NewLicenseResponse
+  error?: string
+  message?: string
+}>
+
+export const POST = async (req: Request): Promise<NewResponse> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    await startDb()
+
+    const licenseData = (await req.json()) as NewLicenseRequest
+    const licenseCode = await generateLicenseKey(32, 8)
+
+    const id = session?.user?.id
+    const product = await ProductModel.findById(licenseData.product_id)
+
+    if (!product) {
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 })
+    }
+    const newLicenseData: NewLicenseRequest = {
+      licenseKey: licenseCode,
+      product_id: product._id,
+      purchasedAt: new Date(),
+      lastChecked: new Date(),
+      user_id: id,
+    }
+
+    const newLicense = await LicenseModel.create({ ...newLicenseData })
+    product.license_id.push(newLicense._id)
+    await product.save()
+
+    return NextResponse.json({
+      license: {
+        id: newLicense._id,
+        licenseKey: newLicense.licenseKey,
+        product_id: newLicense.product_id.toString(),
+        purchasedAt: newLicense.purchasedAt,
+        lastChecked: newLicense.lastChecked,
+        user_id: newLicense.user_id.toString(),
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new license' },
+      { status: 500 }
+    )
+  }
+}
