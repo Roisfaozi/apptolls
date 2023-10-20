11aa960dffccca9cca42f6f50e059c4c826947a6
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 12:42:01 +0700

    * refactor(pageModels.ts): change pageId property to page_id * refactor(pageModels.ts): change content_id property to an array of ObjectId references to 'Content' * refactor(pageModels.ts): change user_id property to an ObjectId reference to 'User'

--- src/models/pageModels.ts
+++ src/models/pageModels.ts
@@ -1,13 +1,13 @@
 import mongoose, { Document, Model, ObjectId, Schema } from 'mongoose'
 const { ObjectId } = mongoose.Schema
 interface IPage extends Document {
-  pageId: string
+  page_id: string
   content_id: ObjectId
   user_id: ObjectId
 }
 
 const pageSchema: Schema<IPage> = new Schema({
-  pageId: { type: String, required: true },
+  page_id: { type: String, required: true },
   content_id: [{ type: ObjectId, ref: 'Content' }],
   user_id: { type: ObjectId, required: true, ref: 'User' },
 })
