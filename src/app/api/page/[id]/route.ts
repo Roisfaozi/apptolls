54fac0b6d4e965c8c3e054f6ae0b71d098cc0225
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 20:58:25 +0700

    * feat(route.ts): add PUT route for updating page data * fix(route.ts): fix error message for page not found * fix(route.ts): fix error message for failed to create new page

--- src/app/api/page/[id]/route.ts
+++ src/app/api/page/[id]/route.ts
@@ -2,6 +2,7 @@ import startDb from '@/lib/db'
 import { getAuthSession } from '@/lib/nextauth-options'
 import { PageModel } from '@/models/pageModels'
 import { NextResponse } from 'next/server'
+import { NewPageRequest, NewResponse } from '../route'
 
 export const GET = async (
   req: Request,
@@ -31,6 +32,43 @@ export const GET = async (
     console.log(error)
     return NextResponse.json(
       { error: 'Failed to find new pages' },
+      { status: 500 }
+    )
+  }
+}
+
+export const PUT = async (
+  req: Request,
+  context: {
+    params: { id: string }
+  }
+): Promise<NewResponse> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    const pageData = (await req.json()) as NewPageRequest
+    await startDb()
+    const id = context.params.id
+    const page = await PageModel.findById(id)
+
+    if (!page) {
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 })
+    }
+
+    page.page_id = pageData.page_id
+    return NextResponse.json({
+      page: {
+        id: page._id.toString(),
+        page_id: page.page_id,
+        user_id: page.user_id.toString(),
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new page' },
       { status: 500 }
     )
   }
