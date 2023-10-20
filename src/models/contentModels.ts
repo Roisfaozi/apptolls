d237f4e04e8914adf4e2fd43eeaff1198aa290e4
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 23:52:21 +0700

    * feat(contentModels.ts): add content model and interface for content documents

---
+++ src/models/contentModels.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Model, ObjectId, Schema } from 'mongoose'
+const { ObjectId } = mongoose.Schema
+
+interface IContent extends Document {
+  title: string
+  description: string
+  isScheduled: boolean
+  publishTime: string
+  publishDate: string
+  page_id: ObjectId
+  user_id: ObjectId
+}
+
+const contentSchema: Schema<IContent> = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  isScheduled: { type: Boolean, required: true },
+  publishTime: { type: String, required: true },
+  publishDate: { type: String, required: true },
+  page_id: { type: ObjectId, required: true, ref: 'Page' },
+  user_id: { type: ObjectId, required: true, ref: 'User' },
+})
+
+const ContentModel: Model<IContent> =
+  mongoose.models['Content'] ||
+  mongoose.model<IContent>('Content', contentSchema)
+
+export { ContentModel }
+export type { IContent }
