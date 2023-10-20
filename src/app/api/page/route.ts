74ed0130705c69c45307384eea46b842157f071e
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 12:42:21 +0700

    * feat(page/route.ts): add POST route to create a new page * - Adds a new file `route.ts` to handle POST requests for creating a new page * - Defines interfaces `NewPageRequest` and `NewPageResponse` to specify the request and response structures * - Implements the `POST` function to handle the POST request * - Checks for authentication session and returns unauthorized response if user is not authenticated * - Validates if the page already exists and returns an error response if it does * - Creates a new page using the `PageModel` and returns the created page in the response

---
+++ src/app/api/page/route.ts
@@ -0,0 +1,55 @@
+import startDb from '@/lib/db'
+import { getAuthSession } from '@/lib/nextauth-options'
+import { PageModel } from '@/models/pageModels'
+import { NextResponse } from 'next/server'
+
+interface NewPageRequest {
+  page_id: string
+  content_id?: string[]
+  user_id: string
+}
+interface NewPageResponse {
+  id: string
+  page_id: string
+  content_id?: string[]
+  user_id?: string
+}
+
+type NewResponse = NextResponse<{
+  page?: NewPageResponse
+  error?: string
+  message?: string
+}>
+// create Page user
+export const POST = async (req: Request): Promise<NewResponse> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    const pageData = (await req.json()) as NewPageRequest
+    await startDb()
+    const oldProduct = await PageModel.findOne({ name: pageData.page_id })
+    if (oldProduct) {
+      return NextResponse.json(
+        { error: 'product is already added!' },
+        { status: 422 }
+      )
+    }
+
+    const newPage = await PageModel.create({ ...pageData })
+    return NextResponse.json({
+      page: {
+        id: newPage._id.toString(),
+        page_id: newPage.page_id,
+        user_id: newPage.user_id.toString(),
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new product' },
+      { status: 500 }
+    )
+  }
+}
