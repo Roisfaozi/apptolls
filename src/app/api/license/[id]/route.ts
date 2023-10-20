23a521fe379a715c66fdf5977855a364decb576f
Roisfaozi <roisfaozi55@gmail.com>
2023-10-20 11:17:54 +0700

    * feat(route.ts): add POST and DELETE methods for license API * fix(route.ts): fix error message in DELETE method for license API

--- src/app/api/license/[id]/route.ts
+++ src/app/api/license/[id]/route.ts
@@ -32,3 +32,61 @@ export const GET = async (
     )
   }
 }
+
+export const POST = async (
+  req: Request,
+  context: { params: { id: string } }
+): Promise<Response> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    await startDb()
+    const id = context.params.id
+    const license = await LicenseModel.findById(id)
+
+    if (!license) {
+      return NextResponse.json({ error: 'License not found' }, { status: 404 })
+    }
+    license.lastChecked = new Date()
+    return NextResponse.json({
+      license,
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new license' },
+      { status: 500 }
+    )
+  }
+}
+
+export const DELETE = async (
+  req: Request,
+  context: { params: { id: string } }
+): Promise<Response> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    await startDb()
+    const id = context.params.id
+    const license = await LicenseModel.findById(id)
+
+    if (!license) {
+      return NextResponse.json({ error: 'License not found' }, { status: 404 })
+    }
+    await license.deleteOne()
+    return NextResponse.json({
+      message: 'License already deleted',
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to create new license' },
+      { status: 500 }
+    )
+  }
+}
