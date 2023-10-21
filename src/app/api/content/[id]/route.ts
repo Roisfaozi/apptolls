72b1e879e24fbcbb8b22733b355abf742590ad30
Roisfaozi <roisfaozi55@gmail.com>
2023-10-21 13:02:05 +0700

    * feat(route.ts): rename variable 'page' to 'content' in GET method * feat(route.ts): add DELETE method to delete content by id

--- src/app/api/content/[id]/route.ts
+++ src/app/api/content/[id]/route.ts
@@ -15,15 +15,39 @@ export const GET = async (
     await startDb()
     const id = context.params.id
 
-    const page = await ContentModel.findOne({ _id: id })
+    const content = await ContentModel.findOne({ _id: id })
       .populate({
         path: 'user_id',
         select: 'id name',
       })
       .populate({ path: 'page_id', select: 'id page_id' })
     return NextResponse.json({
-      page,
+      content,
     })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Failed to find new pages' },
+      { status: 500 }
+    )
+  }
+}
+
+export const DELETE = async (
+  req: Request,
+  context: { params: { id: string } }
+): Promise<Response> => {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new NextResponse('unauthorised', { status: 401 })
+    }
+    await startDb()
+    const id = context.params.id
+
+    const content = await ContentModel.findById(id)
+    await content?.deleteOne()
+    return NextResponse.json({ message: 'Page deleted successfully' })
   } catch (error) {
     console.log(error)
     return NextResponse.json(
