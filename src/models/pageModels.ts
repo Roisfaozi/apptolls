d04e2af007242f09c83e0c5d93dc3b3fd207c163
Roisfaozi <roisfaozi55@gmail.com>
2023-10-29 07:55:10 +0700

    * feat(pageModels.ts): add optional 'name' field to IPage interface * feat(pageModels.ts): add default value for 'name' field in pageSchema

--- src/models/pageModels.ts
+++ src/models/pageModels.ts
@@ -2,12 +2,14 @@ import mongoose, { Model, ObjectId, Schema } from 'mongoose'
 const { ObjectId } = mongoose.Schema
 interface IPage {
   page_id: string
+  name?: string
   content_id?: mongoose.Types.ObjectId[]
   user_id: ObjectId
 }
 
 const pageSchema: Schema<IPage> = new Schema({
   page_id: { type: String, required: true },
+  name: { type: String, default: '' },
   content_id: [{ type: ObjectId, ref: 'Content' }],
   user_id: { type: ObjectId, required: true, ref: 'User' },
 })
