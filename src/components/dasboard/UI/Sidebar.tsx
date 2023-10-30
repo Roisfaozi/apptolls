339e220b6282d3c90a017bacc88e1a3445629a8e
Roisfaozi <roisfaozi55@gmail.com>
2023-10-31 05:02:33 +0700

    * fix(Sidebar.tsx): fix className typo in Sidebar backdrop * feat(Sidebar.tsx): add console.log statement for sidebarExpanded variable * feat(Sidebar.tsx): add onClick event handler for expanding sidebar * feat(Sidebar.tsx): add onClick event handler for expanding sidebar * feat(Sidebar.tsx): add user avatar image

--- src/components/dasboard/UI/Sidebar.tsx
+++ src/components/dasboard/UI/Sidebar.tsx
@@ -63,7 +63,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps): React.ReactNode
     <div>
       {/* Sidebar backdrop (mobile only) */}
       <div
-        className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        className={`fixed inset-0 overflow-hidden bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
           }`}
         aria-hidden="true"
       ></div>
@@ -75,6 +75,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps): React.ReactNode
         className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-64'
           }`}
       >
+
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
           {/* Close button */}
@@ -116,6 +117,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps): React.ReactNode
                           }`}
                         onClick={(e) => {
                           e.preventDefault();
+                          console.log(sidebarExpanded)
                           sidebarExpanded ? handleClick() : setSidebarExpanded(true);
                         }}
                       >
@@ -209,7 +211,10 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps): React.ReactNode
                           }`}
                         onClick={(e) => {
                           e.preventDefault();
+
+
                           sidebarExpanded ? handleClick() : setSidebarExpanded(true);
+
                         }}
                       >
                         <div className="flex items-center justify-between">
@@ -1456,7 +1461,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps): React.ReactNode
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
