bff1391a3ff66cb0df883c14c56358198dae6fb7
Roisfaozi <roisfaozi55@gmail.com>
2023-10-31 02:54:55 +0700

    * fix(SignUpForm.tsx): fix import statement for useRouter from 'next/router' to 'next/navigation'

--- src/components/UI/SignUpForm.tsx
+++ src/components/UI/SignUpForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { ChangeEventHandler, useState } from 'react';
 import Input from '../parts/Input';
 
