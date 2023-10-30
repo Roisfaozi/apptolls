264e76b1bdfdc934631386354592f906c8fc3d34
Roisfaozi <roisfaozi55@gmail.com>
2023-10-31 03:00:52 +0700

    * refactor(layout.tsx): change wrapping <main> element to <></> for better semantics

--- src/app/(guest-route)/layout.tsx
+++ src/app/(guest-route)/layout.tsx
@@ -8,8 +8,8 @@ export default async function AuthLayout({
 }) {
   const session = await getServerSession(authOptions)
   return (
-    <main>
+    <>
       {children}
-    </main>
+    </>
   )
 }
