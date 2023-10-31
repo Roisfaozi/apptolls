d771c7fbcdd8200735555517ee35485a0d7aa41e
Roisfaozi <roisfaozi55@gmail.com>
2023-10-31 13:34:38 +0700

    * feat(tailwind.config.js): add tailwind configuration file with content and theme customization * feat(tailwind.config.js): add tailwind forms plugin * feat(tailwind.config.js): add custom variant for sidebar-expanded class

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import plugin from 'tailwindcss/plugin'
+
+const config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(({ addVariant, e }) => {
+      addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
+        modifySelectors(
+          ({ className }) =>
+            `.sidebar-expanded .${e(
+              `sidebar-expanded${separator}${className}`
+            )}`
+        )
+      })
+    }),
+  ],
+}
+
+export default config
