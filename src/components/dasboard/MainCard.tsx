85092ce32ac9741b046051daac2349ffe3cb87a9
Roisfaozi <roisfaozi55@gmail.com>
2023-11-01 12:43:01 +0700

    *refactor (Maincard.tsx.tsx) component: Replace image and link with icon prop

--- src/components/dasboard/MainCard.tsx
+++ src/components/dasboard/MainCard.tsx
@@ -1,46 +1,26 @@
-import Image, { StaticImageData } from 'next/image';
-import Link from 'next/link';
+import { ReactNode } from 'react';
 
 type MainCardProps = {
   id: string | number
-  link: string;
-  image: HTMLImageElement | StaticImageData;
+  icon: ReactNode;
   name: string;
 };
 
 function MainCard(props: MainCardProps) {
   return (
     <div className="col-span-full sm:col-span-6 xl:col-span-3 bg-white shadow-lg rounded-sm border border-slate-200">
-      <div className="flex flex-col h-full">
-        {/* Card top */}
-        <div className="grow p-5">
-
-          {/* Image + name */}
-          <header>
-            <div className="flex justify-center mb-2">
-              <Link className="relative inline-flex items-start" href={props.link}>
-                <div className="absolute top-0 right-0 -mr-2 bg-white rounded-full shadow" aria-hidden="true">
-                  <svg className="w-8 h-8 fill-current text-amber-500" viewBox="0 0 32 32">
-                    <path
-                      d="M21 14.077a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 010 1.5 1.5 1.5 0 00-1.5 1.5.75.75 0 01-.75.75zM14 24.077a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 110-2 4 4 0 004-4 1 1 0 012 0 4 4 0 004 4 1 1 0 010 2 4 4 0 00-4 4 1 1 0 01-1 1z"
-                    />
-                  </svg>
-                </div>
-                <Image className="rounded-full" src={props.image} width="64" height="64" alt={props.name} />
-              </Link>
-            </div>
-            <div className="text-center">
-              <Link className="inline-flex text-slate-800 hover:text-slate-900" href={props.link}>
-                <h2 className="text-xl leading-snug justify-center font-semibold">{props.name}</h2>
-              </Link>
-            </div>
-
-          </header>
-          {/* Bio */}
-
+      <div className="flex flex-col h-full text-center p-5">
+        <div className="grow mb-1">
+          <div className="inline-flex justify-center items-center w-12 h-12 bg-slate-100 rounded-full mb-2">
+            {props.icon}
+          </div>
+          <h3 className="text-lg text-slate-800 font-semibold mb-1">{props.name}</h3>
+        </div>
+        <div>
+          {/* <a className="text-sm font-medium text-indigo-500 hover:text-indigo-600" href="#0">Explore -&gt;</a> */}
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
