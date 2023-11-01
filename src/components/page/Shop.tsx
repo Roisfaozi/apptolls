07f0aa678c6bf5766977879015f05792fca34120
Roisfaozi <roisfaozi55@gmail.com>
2023-11-02 00:20:16 +0700

    * feat(Product.tsx): Update Product component to display a list of ProductCards

--- src/components/page/Shop.tsx
+++ src/components/page/Shop.tsx
@@ -1,7 +1,27 @@
+import ProductCard from '../UI/ProductCard'
 
 function Product() {
   return (
-    <div>Shop</div>
+    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+      {/* Page header */}
+      <div className="mb-5">
+
+        {/* Title */}
+        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-4">Find the right product for you ✨</h1>
+        <div className="mb-4 border-b border-slate-200"></div>
+
+        <div>
+          <div className="mt-8">
+            <div className="grid grid-cols-12 gap-6">
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
