5ce9d3590ba561dfb044ee4a51267d3e58519df3
Roisfaozi <roisfaozi55@gmail.com>
2023-11-02 01:27:26 +0700

    * feat(Invoices.tsx): add Invoices component

---
+++ src/components/page/Invoices.tsx
@@ -0,0 +1,60 @@
+"use client"
+import { useState } from 'react';
+import DateSelect from '../parts/DateSelect';
+import DeleteButton from '../parts/DeleteButton';
+import FilterButton from '../parts/FilterButton';
+
+function Invoices() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const handleSelectedItems = (selectedItems: string[]) => {
+    setSelectedItems([...selectedItems]);
+  };
+  return (
+    <>
+      <div className="sm:flex sm:justify-between sm:items-center mb-5">
+
+        {/* Left: Title */}
+        <div className="mb-4 sm:mb-0">
+          <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">Invoices ✨</h1>
+        </div>
+
+
+
+      </div>
+      <div className="sm:flex sm:justify-between sm:items-center mb-5">
+        {/* Left side */}
+        <div className="mb-4 sm:mb-0">
+          <ul className="flex flex-wrap -m-1">
+            <li className="m-1">
+              <button className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-transparent shadow-sm bg-indigo-500 text-white duration-150 ease-in-out">All <span className="ml-1 text-indigo-200">67</span></button>
+            </li>
+            <li className="m-1">
+              <button className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out">Paid <span className="ml-1 text-slate-400">14</span></button>
+            </li>
+            <li className="m-1">
+              <button className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out">Due <span className="ml-1 text-slate-400">34</span></button>
+            </li>
+            <li className="m-1">
+              <button className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out">Overdue <span className="ml-1 text-slate-400">19</span></button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Right side */}
+        <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+          {/* Delete button */}
+          <DeleteButton selectedItems={selectedItems} />
+          {/* Dropdown */}
+          <DateSelect />
+          {/* Filter button */}
+          <FilterButton align="right" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Invoices
