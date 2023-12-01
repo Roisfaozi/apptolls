ee50cf616aa249b1c84c326f78a9ddc2f177a5f8
Roisfaozi <roisfaozi55@gmail.com>
2023-12-01 23:27:54 +0700

    Feat: add middleware for fix cros origin

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+
+// the list of all allowed origins
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://reelsmaster.vercel.app',
+]
+
+export function middleware(req: { headers: { get: (arg0: string) => void } }) {
+  const res = NextResponse.next()
+
+  req.headers.get('origin')
+
+  if (allowedOrigins.includes(origin)) {
+    res.headers.append('Access-Control-Allow-Origin', origin)
+  }
+
+  res.headers.append('Access-Control-Allow-Credentials', 'true')
+  res.headers.append(
+    'Access-Control-Allow-Methods',
+    'GET,DELETE,PATCH,POST,PUT'
+  )
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  return res
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
